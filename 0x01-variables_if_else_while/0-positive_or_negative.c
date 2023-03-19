1aaac8e3075f3cea3e5820e962dafd59d21934b1
sheissadi729 <sadicisse729@gmail.com>
2023-03-19 01:41:22 +0000

    0commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - A program that prints a line using printf function
+ * Return: 0 (Sucess)
+ */
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
 if (n > 0)
-{printf("n is positive")}
+	printf("n is positive");
+
 else if (n < 0)
-{printf("n is negative")}
+	printf("n is negative");
+
 else
-{printf("n is zero")}
+	printf("n is zero");
 return (0);
 }
