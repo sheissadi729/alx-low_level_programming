0b2821ca4c8714f47439165239b70345c70f14d1
sheissadi729 <sadicisse729@gmail.com>
2023-03-19 01:45:37 +0000

    0commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,12 +11,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-	printf("n is positive");
+	printf("%d is positive", n);
 
 else if (n < 0)
-	printf("n is negative");
+	printf("%d is negative", n);
 
 else
-	printf("n is zero");
+	printf("%d is zero", n);
 return (0);
 }
