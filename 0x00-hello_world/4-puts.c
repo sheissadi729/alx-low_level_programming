af054b50af60b1a19424a12a4bde254186d03c34
sadio cisse <sadicisse729@gmail.com>
2023-09-07 22:32:06 +0000

    Task 4: make program betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - A program that prints "Programming is like building a multilingual puzzle"
+ * main - A program that prints
+ * "Programming is like building a multilingual puzzle"
  * Return: 0 (success)
  */
 int main(void)
 {
 	char *s = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(s);
 	return (0);
 }
