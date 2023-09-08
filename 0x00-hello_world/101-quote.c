994e6517d8ba3c82cb0886ed9938260fd0f49b65
sadio cisse <sadicisse729@gmail.com>
2023-09-08 02:02:43 +0000

    Task 7 and 8x

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 /**
- * main - A program that prints exactly 
+ * main - A program that prints exactly
  * and that piece of art is useful" - Dora Korpar, 2015-10-19
- * Return: 0 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-	
+	const char *m = ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(STDOUT_FILENO, m, strlen(m));
+	return (1);
 }
