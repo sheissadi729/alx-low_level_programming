b82ddc905e4a07bcf103a5e53d5125eec52f7b58
sadio cisse <sadicisse729@gmail.com>
2023-09-14 16:39:55 +0000

    Fix task 5 and 9 task 5: only use _putchar 3 times task 9: remove space after last word

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,21 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		printf("Fizz ");
-		else if (i % 5 == 0)
-		printf("Buzz ");
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		printf("FizzBuzz ");
+		if (i != 100)
+		{
+			if (i % 3 == 0)
+				printf("Fizz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else if ((i % 3 == 0) && (i % 5 == 0))
+				printf("FizzBuzz ");
+			else
+				printf("%d ", i);
+		}
 		else
-		printf("%d ", i);
+		{
+			printf("Buzz");
+		}
 	}
 	putchar('\n');
 	return (0);
