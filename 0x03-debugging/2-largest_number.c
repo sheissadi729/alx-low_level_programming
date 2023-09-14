1187d972c28262e61897818017e732edc67c3d66
sadio cisse <sadicisse729@gmail.com>
2023-09-14 01:55:25 +0000

    fix bug in task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-	largest = a;
+		largest = a;
 	}
 	else if (b > a && b > c)
 	{
@@ -22,7 +22,7 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
 
 	return (largest);
