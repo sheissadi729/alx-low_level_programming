49078d9c37321494639293f3280db1f9d7027e9d
sadio cisse <sadicisse729@gmail.com>
2023-09-14 16:49:07 +0000

    change order of if statements in task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,12 +15,12 @@ int main(void)
 	{
 		if (i != 100)
 		{
-			if (i % 3 == 0)
+			if ((i % 3 == 0) && (i % 5 == 0))
+				printf("FizzBuzz ");
+			else if (i % 3 == 0)
 				printf("Fizz ");
 			else if (i % 5 == 0)
 				printf("Buzz ");
-			else if ((i % 3 == 0) && (i % 5 == 0))
-				printf("FizzBuzz ");
 			else
 				printf("%d ", i);
 		}
