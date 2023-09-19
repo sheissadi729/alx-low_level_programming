461abcd2cd0ec2c3fe7bcb4df20251d5c6938fdc
sadio cisse <sadicisse729@gmail.com>
2023-09-19 03:00:50 +0000

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer.
+ * @s: pointer to the string
+ * Return: the integer, otherwise 0 if no int in the string
+ */
+int _atoi(char *s)
+{
+	int i;
+	int result = 0;
+	int last_digit;
+	int sign = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			last_digit = s[i] - '0';
+			result = result * 10 + last_digit;
+			if (s[i + 1] > '9' || s[i + 1] < '0')
+			{
+				break;
+			}
+		}
+		else if (s[i] > '9' || s[i] < '0')
+		{
+			continue;
+		}
+	}
+	return (result * sign);
+}
