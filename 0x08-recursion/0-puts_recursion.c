df914f300d35ba9511d8c1eb975d5fee8e210673
sadio cisse <sadicisse729@gmail.com>
2023-09-28 18:46:29 +0000

    fix task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	i++;
-	if (s[i] != '\0')
-		_puts_recursion(s + i);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	else
 		_putchar(10);
 }
