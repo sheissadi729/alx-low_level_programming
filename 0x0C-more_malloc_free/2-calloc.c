e53e5dcf396c1f6f512c030835614156f09bcec2
sheissadi <sadicissse729@gmail.com>
2023-10-08 20:07:09 +0000

    check

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = (malloc(nmemb * size);
+	p = (malloc(nmemb * size));
 	if (p == NULL)
 		return (NULL);
 	else
