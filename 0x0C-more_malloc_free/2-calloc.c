e0b991b4c5de2dca6d91050fad3334dbd091d36e
sheissadi <sadicissse729@gmail.com>
2023-10-09 01:02:35 +0000

    set memory to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,12 +8,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p = NULL;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = (malloc(nmemb * size));
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 	else
-		return (p);
+	{
+		for (i = 0; p[i] != '\0'; i++)
+			p[i] = 0;
+	}
+	return (p);
 }
