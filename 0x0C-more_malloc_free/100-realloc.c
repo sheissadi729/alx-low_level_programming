e43b1332b9078bdf776f873754c06a0345c8130e
sheissadi <sadicissse729@gmail.com>
2023-10-09 02:16:45 +0000

    check

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size == old_size)
 		return (ptr);
-	else if (new_size == 0)
+	else if ((new_size == 0) && (ptr != NULL))
 	{
 		free(ptr);
 		return (NULL);
